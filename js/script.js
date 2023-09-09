7d72e090fa66c407879f4d8cf3f77283bb389318
Cristhian Ceza <cristhiancezardev@gmail.com>
2023-09-09 15:01:31 -0300

    Murando layout da pagina

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-const inforClient = document.getElementById("clientData");
-const addressClient = document.getElementById("inputAddress");
-const inputProduc = document.getElementById("inputProduct");
-
 const cep = document.getElementById("cep").addEventListener("focusout", seachCep)
 const cpf = document.getElementById("cpf").addEventListener("focusout", validationCPF)
 
@@ -69,54 +65,5 @@ async function seachCep() {
         alert("Erro: Cep não encontrado, tente novamente!");
     } else {
         completeAddress(end)
-    }
-}
-const nextPageButton = document.getElementById("next");
-nextPageButton.addEventListener("click", nextPage);
-
-
-function nextPage() {
-    if( document.getElementById("cpf").value == "" ) {
-        next.setAttribute("disabled");
-    }
-
-    if(addressClient.classList.contains("inputAddress") && back.classList.contains("back")) {
-        inforClient.setAttribute("class", "inforPerson");
-        addressClient.removeAttribute('class', 'inputAddress');
-        back.removeAttribute("class", "back");
-
-    } else if(inforClient.classList.contains("inforPerson") && inputProduc.classList.contains("inputProduct")) {
-        addressClient.setAttribute("class", "inputAddress");
-        inputProduc.removeAttribute("class", "inputProduct");
-        next.setAttribute("class", "noNext");
-        send.removeAttribute("class", "send")
-    } else if(!addressClient.classList.contains("inputAddress")) {
-        addressClient.setAttribute("class", "inputAddress");
-        inputProduc.removeAttribute("class", "inputProduct");
-        next.setAttribute("class", "noNext");
-        send.removeAttribute("class", "send")
-    }
-};
-
-
-const backPageButton = document.getElementById("back");
-backPageButton.addEventListener("click", backPage);
-
-function backPage() {
-    if(inforClient.classList.contains("inforPerson") && inputProduc.classList.contains("inputProduct")) {
-        addressClient.setAttribute("class", "inputAddress");
-        back.setAttribute("class", "back");
-        inforClient.removeAttribute("class", "inforPerson");
-
-    } else if(next.classList.contains("noNext") && addressClient.classList.contains("inputAddress")) {
-        send.setAttribute("class", "send");
-        inputProduc.setAttribute("class", "inputAddress");
-        addressClient.removeAttribute("class", "inputAddress");
-        next.removeAttribute("class", "noNext");
-
-    } else if(inputProduc.classList.contains("inputAddress") && send.classList.contains("send")) {
-        addressClient.setAttribute("class", "inputAddress");
-        inforClient.removeAttribute("class", "inforPerson");
-        back.setAttribute("class", "back");
     }
 }
